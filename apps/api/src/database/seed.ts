b8b970371e07dda732841934714a21a2c9022058
Praveen Thanikachalam <100035228+prave01@users.noreply.github.com>
2026-04-02 18:29:14 +0530

    update seed.ts

--- apps/api/src/database/seed.ts
+++ apps/api/src/database/seed.ts
@@ -1,6 +1,6 @@
 import { DatabaseError } from "pg";
 import { db } from "./db";
-import { users, stores, products } from "./schema";
+import { users, stores, products, UserInsert } from "./schema";
 import { SampleUsers, SampleStores, SampleProducts } from "@repo/seed-data";
 
 const seed = async () => {
@@ -8,46 +8,44 @@ const seed = async () => {
     await db.transaction(async (tx) => {
       console.log("Seeding started...");
 
-      const createdUsers = await tx
-        .insert(users)
-        .values(
-          SampleUsers.map((u) => ({
-            name: u.name,
-            email: u.email,
-            password: u.password,
-          })),
-        )
-        .returning({
-          id: users.id,
-          email: users.email,
-        });
+      const buildUsers: UserInsert[] = SampleUsers.map((u) => ({
+        id: u.id,
+        name: u.name,
+        email: u.email,
+        password: u.password,
+      }));
+
+      const createdUsers = await tx.insert(users).values(buildUsers).returning({
+        id: users.id,
+        email: users.email,
+      });
 
       const userMap = new Map(createdUsers.map((u) => [u.email, u.id]));
 
+      const buildStores = SampleStores.map((store) => {
+        const owner = SampleUsers.find((u) => u.id === store.owner_id);
+
+        if (!owner) {
+          throw new Error(`Owner not found for store ${store.name}`);
+        }
+
+        const ownerId = userMap.get(owner.email);
+
+        if (!ownerId) {
+          throw new Error(`User ID missing for ${owner.email}`);
+        }
+
+        return {
+          name: store.name,
+          slug: store.slug,
+          currency: store.currency,
+          ownerId,
+        };
+      });
+
       const createdStores = await tx
         .insert(stores)
-        .values(
-          SampleStores.map((store) => {
-            const owner = SampleUsers.find((u) => u.id === store.owner_id);
-
-            if (!owner) {
-              throw new Error(`Owner not found for store ${store.name}`);
-            }
-
-            const ownerId = userMap.get(owner.email);
-
-            if (!ownerId) {
-              throw new Error(`User ID missing for ${owner.email}`);
-            }
-
-            return {
-              name: store.name,
-              slug: store.slug,
-              currency: store.currency,
-              ownerId,
-            };
-          }),
-        )
+        .values(buildStores)
         .returning({
           id: stores.id,
           slug: stores.slug,
@@ -55,36 +53,36 @@ const seed = async () => {
 
       const storeMap = new Map(createdStores.map((s) => [s.slug, s.id]));
 
+      const buildProducts = SampleProducts.map((product) => {
+        const store = SampleStores.find((s) => s.id === product.store_id);
+
+        if (!store) {
+          throw new Error(`Store not found for product ${product.name}`);
+        }
+
+        const storeId = storeMap.get(store.slug);
+
+        if (!storeId) {
+          throw new Error(`Store ID missing for slug ${store.slug}`);
+        }
+
+        return {
+          name: product.name,
+          category: product.category,
+          price: product.price.toString(),
+          storeId,
+        };
+      });
+
       const createdProducts = await tx
         .insert(products)
-        .values(
-          SampleProducts.map((product) => {
-            const store = SampleStores.find((s) => s.id === product.store_id);
-
-            if (!store) {
-              throw new Error(`Store not found for product ${product.name}`);
-            }
-
-            const storeId = storeMap.get(store.slug);
-
-            if (!storeId) {
-              throw new Error(`Store ID missing for slug ${store.slug}`);
-            }
-
-            return {
-              name: product.name,
-              category: product.category,
-              price: product.price.toString(),
-              storeId,
-            };
-          }),
-        )
+        .values(buildProducts)
         .returning({
           id: products.id,
           name: products.name,
         });
 
-      console.log("✅ Seeding completed");
+      console.log("Seeding completed");
       console.log({
         users: createdUsers.length,
         stores: createdStores.length,
@@ -93,9 +91,9 @@ const seed = async () => {
     });
   } catch (err) {
     if (err instanceof DatabaseError) {
-      console.error("❌ DB Error:", err.message);
+      console.error("DB Error:", err.message);
     } else {
-      console.error("❌ Something went wrong:", err);
+      console.error("Something went wrong:", err);
     }
   }
 };
